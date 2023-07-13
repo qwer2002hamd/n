6ee52f6a1ff8346e35f2d1f83922c893c94fea21
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 19:01:02 +0300

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -8,8 +8,8 @@
 # All rights reserved.
 
 from config import LOG, LOG_GROUP_ID
-from YukkiMusic import app
-from YukkiMusic.utils.database import is_on_off
+from AnonXMusic import app
+from AnonXMusic.utils.database import is_on_off
 
 
 async def play_logs(message, streamtype):
