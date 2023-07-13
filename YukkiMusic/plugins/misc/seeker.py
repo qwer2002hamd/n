e1ba2e34b8aa2f317dc9d9dce97b1f94b9a56d82
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 18:16:47 +0300

    Update seeker.py

--- YukkiMusic/plugins/misc/seeker.py
+++ YukkiMusic/plugins/misc/seeker.py
@@ -12,11 +12,11 @@ import asyncio
 from pyrogram.types import InlineKeyboardMarkup
 
 from strings import get_string
-from YukkiMusic.misc import db
-from YukkiMusic.utils.database import (get_active_chats, get_lang,
+from AnonXMusic.misc import db
+from AnonXMusic.utils.database import (get_active_chats, get_lang,
                                        is_music_playing)
-from YukkiMusic.utils.formatters import seconds_to_min
-from YukkiMusic.utils.inline import (stream_markup_timer,
+from AnonXMusic.utils.formatters import seconds_to_min
+from AnonXMusic.utils.inline import (stream_markup_timer,
                                      telegram_markup_timer)
 
 checker = {}
