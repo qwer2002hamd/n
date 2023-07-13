05c3500bbc7672f77c3c2a699dabd6146380bc1b
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 18:43:01 +0300

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -11,9 +11,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database.memorydatabase import (
+from AnonXMusic import app
+from AnonXMusic.misc import SUDOERS
+from AnonXMusic.utils.database.memorydatabase import (
     get_active_chats, get_active_video_chats)
 
 # Commands
