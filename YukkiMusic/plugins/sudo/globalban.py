e7f1d2970118f6f802adcf4e713cb2ff2b3b9790
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 18:28:44 +0300

    Update globalban.py

--- YukkiMusic/plugins/sudo/globalban.py
+++ YukkiMusic/plugins/sudo/globalban.py
@@ -15,16 +15,16 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils import get_readable_time
-from YukkiMusic.utils.database import (add_banned_user,
+from AnonXMusic import app
+from AnonXMusic.misc import SUDOERS
+from AnonXMusic.utils import get_readable_time
+from AnonXMusic.utils.database import (add_banned_user,
                                        get_banned_count,
                                        get_banned_users,
                                        get_served_chats,
                                        is_banned_user,
                                        remove_banned_user)
-from YukkiMusic.utils.decorators.language import language
+from AnonXMusic.utils.decorators.language import language
 
 # Command
 GBAN_COMMAND = get_command("GBAN_COMMAND")
