9e284be948a9ef6a5a73955b25b7ae04ad1adfd0
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 18:27:35 +0300

    Update blacklistchat.py

--- YukkiMusic/plugins/sudo/blacklistchat.py
+++ YukkiMusic/plugins/sudo/blacklistchat.py
@@ -12,12 +12,12 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (blacklist_chat,
+from AnonXMusic import app
+from AnonXMusic.misc import SUDOERS
+from AnonXMusic.utils.database import (blacklist_chat,
                                        blacklisted_chats,
                                        whitelist_chat)
-from YukkiMusic.utils.decorators.language import language
+from AnonXMusic.utils.decorators.language import language
 
 # Commands
 
