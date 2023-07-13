f681ff2642ead624bae6a8a875dd64c39bf9c026
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 18:26:26 +0300

    Update toptracks.py

--- YukkiMusic/plugins/play/toptracks.py
+++ YukkiMusic/plugins/play/toptracks.py
@@ -13,14 +13,14 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup
 
 from config import BANNED_USERS
-from YukkiMusic import app
-from YukkiMusic.utils.database import (get_global_tops,
+from AnonXMusic import app
+from AnonXMusic.utils.database import (get_global_tops,
                                        get_particulars, get_userss)
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.inline.playlist import (botplaylist_markup,
+from AnonXMusic.utils.decorators.language import languageCB
+from AnonXMusic.utils.inline.playlist import (botplaylist_markup,
                                               failed_top_markup,
                                               top_play_markup)
-from YukkiMusic.utils.stream.stream import stream
+from AnonXMusic.utils.stream.stream import stream
 
 loop = asyncio.get_running_loop()
 
