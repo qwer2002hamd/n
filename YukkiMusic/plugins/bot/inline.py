7338f0fc77aeabe9dacd975d53a3244d7643d3bd
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 18:12:29 +0300

    Update inline.py

--- YukkiMusic/plugins/bot/inline.py
+++ YukkiMusic/plugins/bot/inline.py
@@ -13,8 +13,8 @@ from pyrogram.types import (InlineKeyboardButton,
 from youtubesearchpython.__future__ import VideosSearch
 
 from config import BANNED_USERS, MUSIC_BOT_NAME
-from YukkiMusic import app
-from YukkiMusic.utils.inlinequery import answer
+from AnonXMusic import app
+from AnonXMusic.utils.inlinequery import answer
 
 
 @app.on_inline_query(~BANNED_USERS)
