09dc1ed273d27934a4c797e518d5902094fa69d4
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 18:33:42 +0300

    Update vars.py

--- YukkiMusic/plugins/sudo/vars.py
+++ YukkiMusic/plugins/sudo/vars.py
@@ -13,10 +13,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database.memorydatabase import get_video_limit
-from YukkiMusic.utils.formatters import convert_bytes
+from AnonXMusic import app
+from AnonXMusic.misc import SUDOERS
+from AnonXMusic.utils.database.memorydatabase import get_video_limit
+from AnonXMusic.utils.formatters import convert_bytes
 
 VARS_COMMAND = get_command("VARS_COMMAND")
 
