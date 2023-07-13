f8b29be6f061b244da230e306605450c3a2d6911
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 18:56:18 +0300

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -12,7 +12,7 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton
 
 from config import GITHUB_REPO
-from YukkiMusic import app
+from AnonXMusic import app
 
 
 def start_pannel(_):
