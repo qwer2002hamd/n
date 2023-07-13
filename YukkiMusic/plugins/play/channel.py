de5da9e2f90cba5d0f4cefcf1e661b6874a29ecc
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 18:18:01 +0300

    Update channel.py

--- YukkiMusic/plugins/play/channel.py
+++ YukkiMusic/plugins/play/channel.py
@@ -12,9 +12,9 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database import set_cmode
-from YukkiMusic.utils.decorators.admins import AdminActual
+from AnonXMusic import app
+from AnonXMusic.utils.database import set_cmode
+from AnonXMusic.utils.decorators.admins import AdminActual
 
 ### Multi-Lang Commands
 CHANNELPLAY_COMMAND = get_command("CHANNELPLAY_COMMAND")
