61ffd845d7361258b7a8ccb87aaae2105b092f45
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-13 18:31:36 +0300

    Update logger.py

--- YukkiMusic/plugins/sudo/logger.py
+++ YukkiMusic/plugins/sudo/logger.py
@@ -11,10 +11,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_off, add_on
-from YukkiMusic.utils.decorators.language import language
+from AnonXMusic import app
+from AnonXMusic.misc import SUDOERS
+from AnonXMusic.utils.database import add_off, add_on
+from AnonXMusic.utils.decorators.language import language
 
 # Commands
 LOGGER_COMMAND = get_command("LOGGER_COMMAND")
